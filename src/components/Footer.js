3476dc0c35d2d5bb9c159e30b3e37c9f73916ccb
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-08-05 11:03:13 +0530

    adding Footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,31 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
+      <a href="/"
+        className="youtube social">
         <InstagramIcon />
+      </a>
+        <a href="/"
+        className="youtube social">
         <TwitterIcon />
+        </a>
+        <a href="/"
+        className="youtube social">
         <FacebookIcon />
+        </a>
+        <a href="/"
+        className="youtube social">
         <LinkedInIcon />
+        </a>
       </div>
-      <p> &copy; 2022 pedrotech.com</p>
+      <p> &copy; 2022 Saurabh Kumar Mishra</p>
     </div>
   );
 }
