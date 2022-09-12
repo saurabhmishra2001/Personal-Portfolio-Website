8ef59b6ebaac2012a77458839d0baf4f14c7c04b
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-12 23:05:22 +0530

    changes in project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Projects() {
   return (
     <div>Projects
-      <h1>This page is related to Project</h1>
+      <h1>This page is related to Project
+        <h2>Projects</h2>
+      </h1>
     </div>
   )
 }
