ffe5d475333c8e5acc5304c3dde5b33589e793d8
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-13 14:02:57 +0530

    some changes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,7 +6,9 @@ function Projects() {
       <h1>This page is related to Project
         <h2>Projects</h2>
         <h3>Projects</h3>
-   
+        <p>This page is related to the projects in this projects collection.</p>
+        <p>This page is related to the projects in this projects collection.</p>
+        <p>This page is related to the projects in this projects collection.</p>
       </h1>
     </div>
   )
