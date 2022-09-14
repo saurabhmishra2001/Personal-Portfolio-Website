25c336701033e41717da1976baca73c9ef0c00a0
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-14 14:23:47 +0530

    some changes in navbar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import "../styles/contact.css";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+<div className="jumbotron text-center about">
+    <h1 >Hi, My Name is Saurabh Kumar Mishra</h1>
+    <h4>A software developer with a passion for learning and creating!</h4> 
+ 
+  <div className="prompt">
+          <a href="/">
+            <LinkedInIcon />
+          </a>
+          <a href="/">
+            <EmailIcon />
+          </a>
+          <a href="https://github.com/saurabhmishra2001">
+            <GithubIcon />
+          </a>
+        </div>
+        </div>
+      <div className="container">
+  <div className="row">
+    <div className="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div className="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div className="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+</>
+
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,29 +7,32 @@ import "../styles/Home.css";
 function Home() {
   return (
     <div className="home">
-      <div className="about">
-        <h2> Hi, My Name is Saurabh Kumar Mishra</h2>
-        <div className="prompt">
-          <p>A software developer with a passion for learning and creating.</p>
-          <a href="/"><LinkedInIcon /></a>
-          <a href="/"><EmailIcon /></a>
-          <a href="https://github.com/saurabhmishra2001"><GithubIcon /></a>
+ <div className="jumbotron text-center about">
+    <h1 >Hi, My Name is Saurabh Kumar Mishra</h1>
+    <h4>A software developer with a passion for learning and creating!</h4>
+ 
+  <div className="prompt">
+          <a href="/">
+            <LinkedInIcon />
+          </a>
+          <a href="/">
+            <EmailIcon />
+          </a>
+          <a href="https://github.com/saurabhmishra2001">
+            <GithubIcon />
+          </a>
         </div>
-      </div>
+        </div>
       <div className="skills">
         <h1> Skills</h1>
         <ol className="list">
           <li className="item">
             <h2> Front-End</h2>
-            <span>
-              ReactJS,HTML, CSS, Bootstrap, JavaScript
-            </span>
+            <span>ReactJS,HTML, CSS, Bootstrap, JavaScript</span>
           </li>
           <li className="item">
             <h2>Back-End</h2>
-            <span>
-              MySQL
-            </span>
+            <span>MySQL</span>
           </li>
           <li className="item">
             <h2>Languages</h2>
