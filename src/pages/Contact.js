faae9f3232896b73ece039f13c29eb375296835b
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-14 20:11:54 +0530

    add some pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,27 +1,10 @@
 import React from 'react'
 import "../styles/contact.css";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import EmailIcon from "@material-ui/icons/Email";
-import GithubIcon from "@material-ui/icons/GitHub";
+
 function Contact() {
   return (
     <>
-<div className="jumbotron text-center about">
-    <h1 >Hi, My Name is Saurabh Kumar Mishra</h1>
-    <h4>A software developer with a passion for learning and creating!</h4> 
- <p>nckjegegj</p>
-  <div className="prompt">
-          <a href="/">
-            <LinkedInIcon />
-          </a>
-          <a href="/">
-            <EmailIcon />
-          </a>
-          <a href="https://github.com/saurabhmishra2001">
-            <GithubIcon />
-          </a>
-        </div>
-        </div>
+
       <div className="container">
   <div className="row">
     <div className="col-sm-4">
