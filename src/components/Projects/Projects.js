a45f7b52a7a454012c90ba477a13dc3d9142f947
Saurabh kumar Mishra <saurabhmishra31234@gmail.com>
2024-11-01 21:21:13 +0530

    Add a Project PlanArc

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import ScoreBuzz from "../../Assets/Projects/ScoreBuzz.png";
-
+import PlanArc from "../../Assets/Projects/PlanArc.png";
 
 function Projects() {
   return (
@@ -28,17 +27,15 @@ function Projects() {
           </Col>
 
           <Col md={4} className="project-card">
-          <ProjectCard
-              // imgPath={ScoreBuzz}
+            <ProjectCard
+              imgPath={PlanArc}
               isBlog={false}
               title="ScoreBuzz"
-              description="A Web page that will display the scores of all the crickets that were used to play all over the world. It shows all tournament like IPL and all the internatiolan games, this web page is created using React Js, Node js and some of the CSS libirary like bootstrap and tailwind CSS.  "
-              ghLink="https://github.com/saurabhmishra2001/Cricket_App"
-              // demoLink="https://editor.soumya-jit.tech/"   
+              description="PlanArc is your all-in-one platform designed to streamline the planning and organization of learning, personal projects, and more. With a focus on structure and simplicity, PlanArc helps users map out their learning journeys, set clear goals, and track progress effectively. Whether you’re a student, a professional, or just looking to organize your day-to-day tasks, PlanArc empowers you to manage your time, prioritize tasks, and achieve your goals with ease. Start planning smarter and take control of your learning and productivity with PlanArc.  "
+              ghLink="https://github.com/saurabhmishra2001/PlanArc"
+              demoLink="https://plan-arc-three.vercel.app/"
             />
           </Col>
-
-
         </Row>
       </Container>
     </Container>
